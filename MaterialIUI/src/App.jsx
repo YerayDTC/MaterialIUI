7cfa87a78f08b360649c9a2d04c799dba9f63cd8
yeray <yeray.programacion.web@gmail.com>
2023-11-10 17:05:57 +0000

    header + login

--- MaterialIUI/src/App.jsx
+++ MaterialIUI/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import LoginCard from './components/LoginCard/LoginCard'
+import Pruebas from './components/Pruebas/Pruebas'
 
 function App() {
 
   return (
     <>
       <LoginCard/>
+     {/*  <Pruebas/> */}
     </>
   )
 }
--- MaterialIUI/src/components/LoginCard/LoginCard.jsx
+++ MaterialIUI/src/components/LoginCard/LoginCard.jsx
@@ -1,8 +1,8 @@
-import React, { startTransition, useState } from "react";
-import "./LoginCard.css";
-import { Button, Card, CardActions, CardContent, CardHeader, Divider, Icon, IconButton, InputAdornment, TextField, Typography, useScrollTrigger } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Icon, IconButton, InputAdornment, TextField } from "@mui/material";
 import { blue } from "@mui/material/colors";
 import { Email, Lock, Visibility, VisibilityOff } from "@mui/icons-material";
+import Pruebas from "../Pruebas/Pruebas";
 
 function LoginCard() {
 
@@ -10,17 +10,22 @@ function LoginCard() {
     const [isVisible, setIsVisible] = useState(false)
 
   return (
-    <>
-      <Card
-        sx={{margin:"10px", maxWidth: "700px", backgroundColor: blue[100] }} raised={true}>
-        <CardHeader title="Login"></CardHeader>    
+    <Box sx={{backgroundColor:"primary", width:"40%", marginLeft:"30%"}}>
+        <Pruebas/>
+      {/* <Card
+        sx={{margin:"10px", maxWidth: "700px", backgroundColor: blue[100] }} raised={true}> */}
+      
+   <Card
+        sx={{backgroundColor: blue[100]}} raised={true}>
+        <CardHeader sx={{width:"20px"}} title="Login"></CardHeader>
+
         <CardContent>
             <TextField
             // onChange={(e) => setEmail(e.target.value)}
-            variant="outlined" 
-            fullWidth={true} 
-            margin="dense" 
-            type="text" 
+            variant="outlined"
+            fullWidth={true}
+            margin="dense"
+            type="text"
             label="Email"
             InputProps={{
                 startAdornment: (
@@ -32,12 +37,12 @@ function LoginCard() {
                 )
               }}
             ></TextField>
-            
-            <TextField 
-            variant="outlined" 
-            fullWidth={true} margin="dense" 
-            type={isVisible ? "text":"password"} 
-            label="Paswoord" 
+
+            <TextField
+            variant="outlined"
+            fullWidth={true} margin="dense"
+            type={isVisible ? "text":"password"}
+            label="Paswoord"
             InputProps={{
             startAdornment: (
               <InputAdornment>
@@ -61,10 +66,10 @@ function LoginCard() {
         <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
             <Button size="small" color="warning" variant="contained">Register</Button>
             <Button size="small" color="primary" variant="contained">Login</Button>
-            
+
         </CardActions>
       </Card>
-    </>
+    </Box>
   );
 }
 
